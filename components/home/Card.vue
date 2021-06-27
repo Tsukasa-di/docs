e3237a67d946465b111c5361966b744a2c4a947c
Tsukasa Tomioka <64197521+Tsukasa-di@users.noreply.github.com>
2021-06-27 22:40:49 +0900

    feature/home : Cardクリックのアニメーションを実装、npm CustomEaseの追加

--- components/home/Card.vue
+++ components/home/Card.vue
@@ -1,26 +1,128 @@
 <template lang="pug">
-  div.card(
+  div.card.nonClicked(
     @click='gotoPage'
     :data-pageid='pageId'
-    :class='pageId'
+    ref="card"
   )
-    .card-inner
-      p.title
+    .card-inner(
+      ref="cardInner"
+    )
+      p.title(
+        ref="title"
+        :class='pageId'
+      )
         |{{title}}
-      p.description
+      p.description(
+        ref="description"
+      )
         |{{description}}
 </template>
 
 <script>
+import { gsap } from "gsap";
+import { CustomEase } from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
+
 export default {
   props: {
     pageId: {type: String},
     title: {type: String},
     description: {type: String}
   },
+  data() {
+    return {
+      cards: ""
+    }
+  },
+  mounted() {
+    this.cards = document.querySelectorAll(".card");
+  },
   methods: {
     gotoPage: function(event) {
-      this.$router.push("/gsap/" + event.target.dataset.pageid)
+      // ===================================
+      // Definition
+      // ===================================
+      const VueComponent = this;
+      const unClickedEls = [];
+      this.cards.forEach( card => {
+        if (card.dataset.pageid !== event.target.dataset.pageid) {
+          unClickedEls.push(card);
+        }
+      })
+      function unClickedElsFadeOut() {
+        unClickedEls.forEach( unClickedEl => {
+          gsap.to(unClickedEl, {
+            duration: .5,
+            opacity: 0,
+            y: 50
+          })
+        })
+      }
+      // ===================================
+      // Action Animations
+      // ===================================
+      if (this.$route.name === "gsap") {
+        gsap.timeline()
+          .to(this.$refs.card, {
+            // remove style of card
+            // ---------------------------------
+            duration: .1,
+            border: "none",
+            overflow: "visible",
+            boxShadow: "none",
+            onStart: function() {
+              VueComponent.$refs.card.classList.remove("nonClicked");
+            }
+          })
+          .to(this.$refs.cardInner, {
+            // round off .card-inner
+            // ---------------------------------
+            duration: .5,
+            width: 35,
+            height: 35,
+            borderRadiusPercent: 50,
+            onStart: function() {
+              VueComponent.$refs.cardInner.classList.add(VueComponent.$refs.card.dataset.pageid);
+              unClickedElsFadeOut();
+            }
+          }, "<")
+          .to([this.$refs.title, this.$refs.description], {
+            // fade out .title, .description
+            // ---------------------------------
+            duration: .2,
+            opacity: 0
+          }, "<")
+          .to(this.$refs.cardInner, {
+            // move to left .card-inner
+            // ---------------------------------
+            duration: 1,
+            ease: CustomEase.create("custom", "M0,0 C0.068,0 0.179,-0.14 0.226,-0.16 0.275,-0.181 0.356,-0.228 0.404,-0.186 0.473,-0.125 0.502,0.156 0.518,0.226 0.558,0.404 0.577,0.659 0.616,0.836 0.644,0.965 0.651,1.042 0.705,1.082 0.748,1.114 0.799,1.094 0.817,1.085 0.868,1.061 0.938,0.998 1,1 "),
+            x: ( -this.$refs.cardInner.getBoundingClientRect().right ) + 166,
+            onComplete: function() {
+              VueComponent.$router.push("/gsap/" + VueComponent.$refs.card.dataset.pageid)
+            }
+          })
+          .to(this.$refs.cardInner, {
+            // scale up .card-inner
+            // ---------------------------------
+            duration: .6,
+            ease: "power2.inOut",
+            height: innerHeight,
+            width: 60,
+            x: ( -this.$refs.cardInner.getBoundingClientRect().right ) + 178,
+            borderRadius: 0,
+          }, "-=.2")
+          .to(".header-inner p", {
+            // change color of header paragraph
+            // ---------------------------------
+            duration: .6,
+            color: "#FFFFFF"
+          }, "<")
+        } else {
+          // non Animate expect homepage
+          // ---------------------------------
+          this.$router.push("/gsap/" + VueComponent.$refs.card.dataset.pageid)
+        }
     }
   }
 }
@@ -39,30 +141,38 @@ export default {
   height: 400px;
   border-radius: 20px;
   border: 1px solid map.get(global.$color, sub);
+  box-shadow: 10px 10px 10px map.get(global.$color, sub-thin);
 
   position: relative;
   overflow: hidden;
   cursor: pointer;
+  transition: .3s;
+  transform-origin: top left;
 
-  &.getting-started {
-    .title {
-      background-color: map.get(global.$color, _yellow);
-    }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &.nonClicked:hover {
+    box-shadow: none;
+    transform: translate3d(5px, 5px, 0);
   }
-  &.learning {
-    .title {
-      background-color: map.get(global.$color, _blue);
-    }
+
+  .getting-started {
+    background-color: map.get(global.$color, _yellow);
   }
-  &.samples {
-    .title {
-      background-color: map.get(global.$color, _red);
-    }
+  .learning {
+    background-color: map.get(global.$color, _blue);
+  }
+  .samples {
+    background-color: map.get(global.$color, _red);
   }
 }
 
 .card-inner {
   position: absolute;
+  overflow: hidden;
+
   width: 100%;
   height: 100%;
   border-radius: 20px;
