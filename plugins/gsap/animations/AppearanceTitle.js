b49c490b034f92ffa8334394404bf8cd9e4c2d33
Tsukasa Tomioka <64197521+Tsukasa-di@users.noreply.github.com>
2021-06-27 18:11:47 +0900

    feature/home : Webサイト訪問時の初回のちらつきを修正するため出現アニメーションを実装

--- plugins/gsap/animations/AppearanceTitle.js
+++ plugins/gsap/animations/AppearanceTitle.js
@@ -2,11 +2,11 @@ import { gsap } from "gsap";
 import "../effects";
 
 export function AppearanceTitle(duration=1, y=50) {
-  const AppearanceTitle = gsap.timeline()
-    .fadeIn("body", {duration: 1})
-    .fadeUp(".docs", {duration: duration, y: y}, "-=.5")
-    .fadeUp(".green-sock", {duration: duration, y: y}, "-=.1")
-    .fadeUp(".animation-platform", {duration: duration, y: y}, "-=.1")
+  gsap.timeline({ defaults: {duration: duration} })
+    .to("body", {opacity: 1, ease: "power4.in"})
+    .fadeUp(".docs", {y: y}, "-=.5")
+    .fadeUp(".green-sock", {y: y}, "-=.1")
+    .fadeUp(".animation-platform", {y: y}, "-=.1")
     .fadeIn(".G", {duration: .4,})
     .fadeIn(".S", {duration: .4,}, "-=.1")
     .fadeIn(".A", {duration: .4,}, "-=.1")
