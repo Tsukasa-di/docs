2189e5b91566d33030e49260a9bd04375fb171ef
Tsukasa Tomioka <64197521+Tsukasa-di@users.noreply.github.com>
2021-06-28 19:01:17 +0900

    vue-svg-loaderの導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,16 @@ export default {
           fiber: FIBER
         }
       }
+    },
+    extend: (config) => {
+      const svgRule = config.module.rules.find(rule => rule.test.test('.svg'));
+
+      svgRule.test = /\.(png|jpe?g|gif|webp)$/;
+
+      config.module.rules.push({
+        test: /\.svg$/,
+        loader: 'vue-svg-loader',
+      });
     }
   }
 }
