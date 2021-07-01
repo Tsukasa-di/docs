310cb4fe070c868d1831117d1d2a216f1cf8f80d
Tsukasa Tomioka <64197521+Tsukasa-di@users.noreply.github.com>
2021-07-02 03:18:41 +0900

    feature/getting-started : 見出し「GSAPの書き方」の途中まで作成

--- pages/gsap/getting-started/index.vue
+++ pages/gsap/getting-started/index.vue
@@ -13,8 +13,8 @@
           p
             |GSAP(GreenSock Animation Platfrom)は、Flash全盛の時代から利用され続けている歴史ある老舗アニメーションライブラリ。その使用実績は、公式情報で10,000,000サイトほどの超絶な人気を誇っています。
           br
-          p
-            |<strong>GSAPが使われているサイト達</strong>
+          h4
+            |GSAPが使われているサイト達
           .column-2.margin-bottom-30
             a(
               class="image-mask"
@@ -50,7 +50,7 @@
           p
             |この簡単な記述で楽々実装。色んなサイトからインスピも得られれば表現の幅も広がる！
             br
-            |早速インストールして試していきましょう〜！
+            |早速インストールして試していきましょう！
         section
           h2(
             id="Basic"
@@ -64,17 +64,17 @@
             |CDNからのダウンロード、NPM/Yarnでのインストール。
           <pre class="prettyprint prettyprinted" style=""><span class="tag">&lt;script</span><span class="pln"> </span><span class="atn">src</span><span class="pun">=</span><span class="atv">"https://cdnjs.cloudflare.com/ajax/libs/gsap/3.7.0/gsap.min.js"</span><span class="tag">&gt;&lt;/script&gt;</span></pre>
           pre(
-            class="prettyprint lang-js linenums"
+            class="prettyprint lang-js"
           )
             |npm install gsap
           pre(
-            class="prettyprint lang-js linenums"
+            class="prettyprint lang-js"
           )
             |yarn add gsap
           p
             |インストール後、次のようにプロジェクトに組み込み。
           pre(
-            class="prettyprint lang-js linenums"
+            class="prettyprint lang-js"
           )
             |import { gsap } from "gsap";
           p
@@ -118,6 +118,49 @@
             id="GSAPの書き方"
           )
             |GSAPの書き方
+          p
+            |紹介してきたgsap.to()やgsap.from()のような指定した要素をアニメーションさせるメソッドの基本的な記述方法を説明していきます。アニメーションをするために必要な引数は2つ。
+          pre(
+            class="prettyprint lang-js linenums"
+          )
+            |gsap.to(targets, setting)
+            |setting = {
+            |    duration: .5,
+            |    x: 200, // transform: translateX(100px)
+            |    opacity: 0,
+            |})
+          .wrapper-bg.margin-bottom-30
+            ul.margin-bottom-0
+              li
+                |<strong>targets</strong><br>アニメイトさせたいHTML要素やオブジェクトを渡します。<code>.myClass</code>のようにclassやidなどノードを特定したいStringを渡したり、Arrayで複数のノードを指定することもできます。
+              li
+                |<strong>setting</strong><br>どのようにアニメイトさせるか設定するための値をオブジェクト型で入力します。
+          h4
+            |<code>targets</code>に渡せられる値について
+          p
+            |基本的には<code>.myClass</code>のように、ノードを指定するためのclassやidなどをStringで渡せば、アニメイトさせたい要素を特定してくれます。
+          <p class="codepen" data-height="300" data-default-tab="js,result" data-slug-hash="xxdbggb" data-editable="true" data-user="tsukasa-di" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+            <span>See the Pen <a href="https://codepen.io/tsukasa-di/pen/xxdbggb">
+            </a> by Tsukasa-di (<a href="https://codepen.io/tsukasa-di">@tsukasa-di</a>)
+            <a href="https://codepen.io">CodePen</a>.</span>
+          </p>
+          <script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+          br
+          p
+            |そして、GSAPで操ることのできるのはCSSだけではありません。下記のようにオブジェクトを指定すると、そのオブジェクトの値を操ることだってできます。
+          <p class="codepen" data-height="300" data-default-tab="js,result" data-slug-hash="PomqWNW" data-editable="true" data-user="tsukasa-di" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+            <span>See the Pen <a href="https://codepen.io/tsukasa-di/pen/PomqWNW">
+            </a> by Tsukasa-di (<a href="https://codepen.io/tsukasa-di">@tsukasa-di</a>)
+            <a href="https://codepen.io">CodePen</a>.</span>
+          </p>
+          br
+          <script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+          p
+            |上記の例では、<code>targets</code>にオブジェクト（<code>calcNumber</code>）を入れることで、そのオブジェクトの値をも、アニメーションさせています。
+          p
+            |<code>setting</code>のプロパティに、<code>targets</code>で指定したオブジェクトのプロパティ（<code>currentNumber</code>）を設定し、その値にどの数値までアニメーションさせたいか目標値（<code>currentNumber: 100</code>）を入れてやることで、オブジェクトの値を操作することができます。
+          h4
+            |<code>setting</code>のプロパティについて
           h3(
             id="gsap.timeline()"
           )
@@ -221,7 +264,6 @@ export default {
     this.indexes = MakeIndexArray();
   }
 }
-
 </script>
 
 <style lang="scss" scoped>
